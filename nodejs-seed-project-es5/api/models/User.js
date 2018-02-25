92713e7334e391d9bf626639548933576b9db054
ShadyTantawy <ShadyTantawy@protonmail.com>
2018-02-25 17:59:35 +0200

    [+] Hashing passwords feature

--- nodejs-seed-project-es5/api/models/User.js
+++ nodejs-seed-project-es5/api/models/User.js
@@ -1,10 +1,25 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 var userSchema = new Schema({
     username: { type: String, required: true, trim: true, unique: true},
     password: { type: String, required: true, trim: true},
     email:    { type: String, lowercase: true, unique: true}
 });
+
+userSchema.pre('save', async function(next) {
+    try {
+      // Generate a salt
+      const salt = await bcrypt.genSalt(10);
+      // Generate a password hash (salt + hash)
+      const passwordHash = await bcrypt.hash(this.password, salt);
+      // Saving the hash in the password attribute
+      this.password = passwordHash;
+      next();
+    } catch(error) {
+      next(error);
+    }
+  });
   
 mongoose.model('User', userSchema);
