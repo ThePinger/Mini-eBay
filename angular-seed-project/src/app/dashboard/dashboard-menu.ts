c95ee8ea44b1e54a89ddba177f032f020292c560
ShadyTantawy <ShadyTantawy@protonmail.com>
2018-02-28 01:43:14 +0200

    [*] Modified dashboard-menu
    Menu items:
    -Dashboard
    -Store
    -Cart

--- angular-seed-project/src/app/dashboard/dashboard-menu.ts
+++ angular-seed-project/src/app/dashboard/dashboard-menu.ts
@@ -1,10 +1,20 @@
 import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  { title: 'Dashboard', icon: 'nb-home', link: '/dashboard', home: true },
+  { 
+    title: 'Dashboard', 
+    icon: 'nb-home', 
+    link: '/user/dashboard', 
+    home: true 
+  },
   {
-    title: 'Items',
+    title: 'Store',
     icon: 'fa fa-shopping-basket',
-    link: '/dashboard/items'
+    link: '/user/store/products'
+  },
+  {
+    title: 'Cart',
+    icon: '',
+    link: '/user/store/cart'
   }
 ];
