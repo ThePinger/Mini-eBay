1d900b9b3547b4f829a2ab416db5b47cc5c02607
Amr Mostafa Elshehaly <amshehaly@gmail.com>
2018-02-28 20:48:37 +0200

    Added SignUp front-End

--- angular-seed-project/src/app/authentication/signup/signup.component.ts
+++ angular-seed-project/src/app/authentication/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  //@Input emailenter : string;
+
+   
   constructor() { }
+
+
 
   ngOnInit() {
   }
