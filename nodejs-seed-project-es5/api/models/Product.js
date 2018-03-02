7470ae22d63a163c874189e93ed68e9358dea900
abdellatiif <17496863+abdellatiif@users.noreply.github.com>
2018-03-02 22:54:38 +0200

    Update Product.js

--- nodejs-seed-project-es5/api/models/Product.js
+++ nodejs-seed-project-es5/api/models/Product.js
@@ -35,4 +35,5 @@ Product.create({name: 'PlayStation', price:'4000', seller:'Omar Emad'});
 
 Product.create({name: 'SheeshTawook', price:'2014', seller:'Abdellatiif'});
 Product.create({name: 'Shawerma', price:'1084', seller:'Abdellatiif'});
-Product.create({name: 'Hamada', price:'1065', seller:'Abdellatiif'});
+Product.create({name: 'Apple Pie', price:'1065', seller:'Abdellatiif'});
+Product.create({name: 'WaterMelon', price:'1065', seller:'Abdellatiif'});
