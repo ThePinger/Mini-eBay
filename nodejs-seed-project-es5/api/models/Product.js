4cda7d1f199ce279cbe4aa1689bd10ca7c997f0b
Unknown <amgadramses@yahoo.com>
2018-03-02 22:43:57 +0200

    Added 3 products to my individual part

--- nodejs-seed-project-es5/api/models/Product.js
+++ nodejs-seed-project-es5/api/models/Product.js
@@ -17,7 +17,24 @@ var productSchema = new Schema({
     type: Date,
     default: Date.now
   },
-  updatedAt: Date
+  updatedAt: Date,
+  seller: {
+    type: Schema.Types.String, ref: 'User'
+  }
 });
 
 mongoose.model('Product', productSchema);  
+var Product = mongoose.model('Product');
+
+Product.create({name: 'HamBurger', price:'2000', seller:'Omar Emad'});
+Product.create({name: 'Hotdog', price:'3000', seller:'Omar Emad'});
+Product.create({name: 'PlayStation', price:'4000', seller:'Omar Emad'});
+
+Product.create({name: 'Swatch', price:'3500', seller:'Amgad Ashraf'});
+Product.create({name: 'HP Laptop', price:'19000', seller:'Amgad Ashraf'});
+Product.create({name: 'Samsung TV', price:'17500', seller:'Amgad Ashraf'});
+
+
+
+
+//---------------------ADD YOUR PRODUCTS HERE----------------------------
