b8ec2a79a0a7797fc35be3945c86daa016510e3f
Unknown <yasmina_elsaket@hotmail.com>
2018-03-02 16:06:28 +0200

    store component

--- angular-seed-project/src/app/store/store.component.ts
+++ angular-seed-project/src/app/store/store.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { MENU_ITEMS } from './store-menu';
+
+import { Router } from '@angular/router';
+// import { Ng2SmartTableModule } from 'ng2-smart-table';
+
+import { NbMenuItem } from '@nebular/theme/components/menu/menu.service';
+import { StoreService } from '../service/store.service';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.scss']
+  styleUrls: ['./store.component.scss'],
+ template: '<ng2-smart-table [Myprodcuts]="Myproducts"></ng2-smart-table>'
 })
+
 export class StoreComponent implements OnInit {
 
-  constructor() { }
+  createresponse;
+  updateresponse;
+  deleteresponse;
 
+
+  constructor(private storeService:StoreService, private router: Router ) {} 
   ngOnInit() {
   }
+    async createProduct(name, price){
+    await this.storeService.createProduct(name, price).subscribe((response => {this.router.navigate(['/user/store/products'])}), (err => {this.createresponse = "Can not create product"}));
+  }
+  async updateProduct(){
+    await this.storeService.updateProduct().subscribe((response => {this.router.navigate(['/user/store/products'])}), (err => {this.createresponse = "Can't update product"}));
+  }
+  async deleteProduct(){
+    await this.storeService.deleteProduct().subscribe((response => {this.router.navigate(['/user/store/products'])}), (err => {this.deleteresponse = "Can't delete product"}));
+  }
+ 
+
 
 }
