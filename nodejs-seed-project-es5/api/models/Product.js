217fb746d55ca73d7274d4c5817039e10bb15d00
Unknown <akramashraf96@gmail.com>
2018-03-02 22:31:46 +0200

    Individual part done

--- nodejs-seed-project-es5/api/models/Product.js
+++ nodejs-seed-project-es5/api/models/Product.js
@@ -31,3 +31,7 @@ Product.create({name: 'Hotdog', price:'3000', seller:'Omar Emad'});
 Product.create({name: 'PlayStation', price:'4000', seller:'Omar Emad'});
 
 //---------------------ADD YOUR PRODUCTS HERE----------------------------
+
+Product.create({name: 'Samsung', price:'5000', seller:'Akram Ashraf'});
+Product.create({name: 'Oneplus', price:'8000', seller:'Akram Ashraf'});
+Product.create({name: 'Rolex', price:'12000', seller:'Akram Ashraf'});
