10bf5ae1b2a019d73d7cb6bb5596305805106998
ThePinger <omaremadsabry@gmail.com>
2018-03-02 10:39:41 +0200

    Updated Dashboard and Parent Component
    Potato Contributed

--- angular-seed-project/src/app/parent/parent.component.ts
+++ angular-seed-project/src/app/parent/parent.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MENU_ITEMS } from './../dashboard/dashboard-menu';
-
 import { NbMenuItem } from '@nebular/theme/components/menu/menu.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-parent',
@@ -11,10 +12,12 @@ import { NbMenuItem } from '@nebular/theme/components/menu/menu.service';
 export class ParentComponent implements OnInit {
   menu: NbMenuItem[];
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
-    this.menu = MENU_ITEMS;
+  ngOnInit() 
+  {
+      this.authService.isLoggedIn().subscribe(msg => {}, err => {this.router.navigate([''])});
+      this.menu = MENU_ITEMS;
   }
 
 }
