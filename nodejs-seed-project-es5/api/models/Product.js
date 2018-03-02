b724935c3d8e0dfd2cb22069e73e1f64b8f8f11f
Unknown <yasmina_elsaket@hotmail.com>
2018-03-02 22:05:06 +0200

    Products added

--- nodejs-seed-project-es5/api/models/Product.js
+++ nodejs-seed-project-es5/api/models/Product.js
@@ -21,3 +21,14 @@ var productSchema = new Schema({
 });
 
 mongoose.model('Product', productSchema);  
+var Product = mongoose.model('Product');
+
+Product.create({name: 'Laptop', price:'9000', seller:'Yasmina Elsaket'});
+Product.create({name: 'Pandora', price:'7000', seller:'Yasmina Elsaket'});
+Product.create({name: 'Jacket', price:'1000', seller:'Yasmina Elsaket'});
+Product.create({name: 'New Balance', price:'2000', seller:'Yasmina Elsaket'});
+Product.create({name: 'Kitkat', price:'10', seller:'Yasmina Elsaket'});
+Product.create({name: 'Bag', price:'1500', seller:'Yasmina Elsaket'})
+Product.create({name: 'Sleeve', price:'200', seller:'Yasmina Elsaket'});
+Product.create({name: 'Basket', price:'300', seller:'Yasmina Elsaket'});
+Product.create({name: 'TV', price:'10000', seller:'Yasmina Elsaket'});
